f7e08d6843361fa1616c463bd0110bce978d4c20
dashavpav <dashavpavlov@gmail.com>
2019-05-27 12:42:52 +0300

    With_commits

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 main(){
-string name;
+string name;//комментарий
 std::cin>>name;
-std::cout<<"Hello World from "<<name;
+std::cout<<"Hello World from "<<name;//комментарий
 }
 
